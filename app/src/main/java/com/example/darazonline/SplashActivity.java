6f45861d3bf0d1cae14999a44a5eccee9d89ee36
bhattarai <bhattarai.utk1@gmail.com>
2019-12-26 08:48:03 +0545

    Adding Splash Activity

---
+++ app/src/main/java/com/example/darazonline/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.darazonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView=findViewById(R.id.imgdaraz);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
+        RotateAnimation rotate = new RotateAnimation(30, 360, Animation.RELATIVE_TO_SELF, 0.5f,  Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(4000);
+        imageView.startAnimation(rotate);
+
+    }
+}
